a4809c87d2371b6914efd44673d5761ba1957ba1
Surya J <147895125+smileysurya@users.noreply.github.com>
2025-10-27 02:55:06 +0530

    Delete task 2/serverless-email-api directory

--- task 2/serverless-email-api/config.js
+++
@@ -1,7 +0,0 @@
-// Load environment variables for local development
-require('dotenv').config();
-
-module.exports = {
-  EMAIL_USER: process.env.EMAIL_USER,
-  EMAIL_PASS: process.env.EMAIL_PASS,
-};
